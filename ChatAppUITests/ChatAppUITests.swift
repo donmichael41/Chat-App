c5e5e26d70825f3c89cc4b62293312cf69d9dd21
Hung Nguyen <donmichael1Root@donmichaels-MacBook-Pro.local>
2016-12-11 22:14:52 +0700

    Add more UI test

--- ChatAppUITests/ChatAppUITests.swift
+++ ChatAppUITests/ChatAppUITests.swift
@@ -12,22 +12,11 @@ class ChatAppUITests: XCTestCase {
         
     override func setUp() {
         super.setUp()
-        
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-        
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
 
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-        
+        continueAfterFailure = false
+        XCUIApplication().launch()
     }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
+
     
     func test_EnterShortName_ShouldNotEnableButton() {
         XCUIDevice.shared().orientation = .portrait
@@ -62,6 +51,22 @@ class ChatAppUITests: XCTestCase {
         
         let yourButton = app.buttons["Let's Go"]
         XCTAssertTrue(yourButton.isEnabled)
+    }
+    
+    
+    func test_SendMessage() {
+        XCUIDevice.shared().orientation = .faceUp
+        XCUIDevice.shared().orientation = .faceUp
+        XCUIDevice.shared().orientation = .portrait
+        
+        let app = XCUIApplication()
+        let yourNameTextField = app.textFields["Your name"]
+        yourNameTextField.tap()
+        
+        let moreKey = app.keys["more"]
+        moreKey.tap()
+        moreKey.tap()
+        yourNameTextField.typeText("123456")
         
         app.buttons["Let's Go"].tap()
         
@@ -69,11 +74,21 @@ class ChatAppUITests: XCTestCase {
         let newMessageTextView = toolbarsQuery.textViews["New Message"]
         newMessageTextView.tap()
         app.keys["H"].tap()
-        app.keys["u"].tap()
         newMessageTextView.typeText("Hung")
         toolbarsQuery.buttons["Send"].tap()
-        XCUIDevice.shared().orientation = .faceUp
         
+         newMessageTextView.tap()
+        newMessageTextView.typeText("Test")
+        toolbarsQuery.buttons["Send"].tap()
+        
+        
+        newMessageTextView.tap()
+        newMessageTextView.typeText("Done")
+        toolbarsQuery.buttons["Send"].tap()
+        
+        
+        newMessageTextView.tap()
+        newMessageTextView.typeText("Thank you")
+        toolbarsQuery.buttons["Send"].tap()
     }
-    
 }
