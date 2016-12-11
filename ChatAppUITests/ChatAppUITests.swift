b47e6708a1bff7481da3f19166f237415b0dc6a3
Hung Nguyen <donmichael1Root@donmichaels-MacBook-Pro.local>
2016-12-11 22:03:39 +0700

    Add UI test for LoginViewController

--- ChatAppUITests/ChatAppUITests.swift
+++ ChatAppUITests/ChatAppUITests.swift
@@ -18,7 +18,7 @@ class ChatAppUITests: XCTestCase {
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        //XCUIApplication().launch()
+        XCUIApplication().launch()
 
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
         
@@ -29,9 +29,51 @@ class ChatAppUITests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func test_EnterShortName_ShouldNotEnableButton() {
+        XCUIDevice.shared().orientation = .portrait
+        XCUIDevice.shared().orientation = .portrait
+        
+        let app = XCUIApplication()
+        let yourNameTextField = app.textFields["Your name"]
+        yourNameTextField.tap()
+        
+        let moreKey = app.keys["more"]
+        moreKey.tap()
+        moreKey.tap()
+        yourNameTextField.typeText("12345")
+        
+        let yourButton = app.buttons["Let's Go"]
+        XCTAssertFalse(yourButton.isEnabled)
+    }
+    
+    func test_EnterEnoughName_ShouldEnableButton() {
+        XCUIDevice.shared().orientation = .faceUp
+        XCUIDevice.shared().orientation = .faceUp
+        XCUIDevice.shared().orientation = .portrait
+        
+        let app = XCUIApplication()
+        let yourNameTextField = app.textFields["Your name"]
+        yourNameTextField.tap()
+        
+        let moreKey = app.keys["more"]
+        moreKey.tap()
+        moreKey.tap()
+        yourNameTextField.typeText("123456")
+        
+        let yourButton = app.buttons["Let's Go"]
+        XCTAssertTrue(yourButton.isEnabled)
+        
+        app.buttons["Let's Go"].tap()
+        
+        let toolbarsQuery = app.toolbars
+        let newMessageTextView = toolbarsQuery.textViews["New Message"]
+        newMessageTextView.tap()
+        app.keys["H"].tap()
+        app.keys["u"].tap()
+        newMessageTextView.typeText("Hung")
+        toolbarsQuery.buttons["Send"].tap()
+        XCUIDevice.shared().orientation = .faceUp
+        
     }
     
 }
